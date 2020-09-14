ada57c91adc251d536da2abe53be5d9c8c4a6d6e
赵宁 <56795657@qq.com>
2020-09-14 10:33:52 +0800

    宁

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    吴孟林
+                    宁
                 </div>
 
                 <div class="links">
