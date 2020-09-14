40ff7671dd13692fcd5826d3b932ada2b93bb0fa
lin <1501113246@qq.com>
2020-09-15 07:44:52 +0800

    完善

--- resources/views/index/layouts/left.blade.php
+++ resources/views/index/layouts/left.blade.php
@@ -20,12 +20,12 @@
         <dl>
             <dt><i>·</i> 我的中心</dt>
             <dd><a href="home-person-collect.html" >我的收藏</a></dd>
-            <dd><a href="home-person-collect.html" >优惠卷</a></dd>
-            <dd><a href="home-person-footmark.html" >我的足迹</a></dd>
+            <dd><a href="{{url('man/coupon')}}" >优惠卷</a></dd>
+            <dd><a href="{{(url('man/history'))}}" >我的足迹</a></dd>
         </dl>
         <dl>
             <dt><i>·</i> 设置</dt>
-            <dd><a href="home-setting-info.html" class="list-active">个人信息</a></dd>
+            <dd><a href="{{url('man/perinfo')}}" class="list-active">个人信息</a></dd>
             <dd><a href="home-setting-address.html"  >地址管理</a></dd>
             <dd><a href="home-setting-safe.html" >安全管理</a></dd>
         </dl>
--- resources/views/man/history.blade.php
+++ resources/views/man/history.blade.php
@@ -11,41 +11,7 @@
     <div class="py-container">
         <div class="yui3-g collect">
             <!--左侧列表-->
-            <div class="yui3-u-1-6 list">
-
-                <div class="person-info">
-                    <div class="person-photo"><img src="/asses/img/_/photo.png" alt=""></div>
-                    <div class="person-account">
-                        <span class="name">Michelle</span>
-                        <span class="safe">账户安全</span>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="list-items">
-                    <dl>
-                        <dt><i>·</i> 订单中心</dt>
-                        <dd ><a href="home-index.html"   >我的订单</a></dd>
-                        <dd><a href="home-order-pay.html" >待付款</a></dd>
-                        <dd><a href="home-order-send.html"  >待发货</a></dd>
-                        <dd><a href="home-order-receive.html" >待收货</a></dd>
-                        <dd><a href="home-order-evaluate.html" >待评价</a></dd>
-                    </dl>
-                    <dl>
-                        <dt><i>·</i> 我的中心</dt>
-                        <dd><a href="home-person-collect.html" >我的收藏</a></dd>
-                        <dd><a href="home-person-footmark.html" class="list-active">我的足迹</a></dd>
-                    </dl>
-                    <dl>
-                        <dt><i>·</i> 物流消息</dt>
-                    </dl>
-                    <dl>
-                        <dt><i>·</i> 设置</dt>
-                        <dd><a href="home-setting-info.html">个人信息</a></dd>
-                        <dd><a href="home-setting-address.html"  >地址管理</a></dd>
-                        <dd><a href="home-setting-safe.html" >安全管理</a></dd>
-                    </dl>
-                </div>
-            </div>
+            @include("index.layouts.left")
             <!--右侧主内容-->
             <div class="yui3-u-5-6 goods">
                 <div class="body">
